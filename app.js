4bbf6464ba672bdc97e8980c852744dcbe4414f2
alex090890 <alexprofteach@gmail.com>
2024-03-19 10:48:12 +0100

    Added root route

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const port = 8001
 const pets = require('./petList');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send(`
+        <h1>Adopt a Pet!</h1>
+        <p>Browse through the links below to find your new furry friend</p>
+        <ul>
+            <li>Dogs</li>
+            <li>Cats</li>
+            <li>Rabbits</li>
+        </ul>
+    `)
 })
 
 app.get('/pets', (req, res) => {
