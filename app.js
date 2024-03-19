6a366d4db1e102318c9cd10e372d19f2eb41891f
alex090890 <alexprofteach@gmail.com>
2024-03-19 11:03:16 +0100

    Worked with the routes

--- app.js
+++ app.js
@@ -8,12 +8,25 @@ app.get('/', (req, res) => {
         <h1>Adopt a Pet!</h1>
         <p>Browse through the links below to find your new furry friend</p>
         <ul>
-            <li>Dogs</li>
-            <li>Cats</li>
-            <li>Rabbits</li>
+            <li><a href="/animals/dogs">Dogs</a></li>
+            <li><a href="/animals/cats">Cats</a></li>
+            <li><a href="/animals/rabbits">Rabbits</a></li>
         </ul>
     `)
-})
+});
+app.get('/animals/:pet_type', (req, res) => {
+  const petType = req.params.pet_type;
+  if (pets[petType]) {
+    let html = `<h1>List of ${petType}</h1><ul>`;
+    pets[petType].forEach(pet => {
+        html += `<li>${pet.name} - ${pet.breed}</li>`;
+    });
+    html += '</ul>';
+    res.send(html);
+  } else {
+    res.status(404).send({ message: 'Pet type not found' });
+  }
+});
 
 app.get('/pets', (req, res) => {
   res.json(pets);
